d9213b204a23db5466e4c827906204add2245ce1
TommiON <tommi.niittymies@gmail.com>
2025-02-15 21:49:06 +0200

    Toimimaton sequelize pois

--- back/index.ts
+++ back/index.ts
@@ -2,10 +2,8 @@ import express, { Application } from 'express';
 import cors from 'cors';
 
 import { PORT } from './utils/config';
-import { connectToDatabase } from './utils/database';
 
 import healthCheckRouter from './routes/healthCheckRouter';
-import clubRouter from './routes/clubRouter';
 
 const app: Application = express();
 
@@ -13,11 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/healthcheck', healthCheckRouter);
-// app.use('/api/club', clubRouter);
+// kun alla olevan palauttaa, tietokantaongelmat palaavat
+//app.use('/api/club', clubRouter);
 
 const start = async () => {
-  console.log('onko tässä?')
-  await connectToDatabase();
   app.listen(PORT, () => {
     console.log(`Sovellus käynnissä ja vastaa portissa ${PORT}`);
   });
